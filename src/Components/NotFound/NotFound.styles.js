e1ab214cd2bee073fa8460adc559fda3269f3453
Dinkar <aakashdinkarh@gmail.com>
2024-10-16 19:35:45 +0530

    horizontal center and margin top for 404 content

--- src/Components/NotFound/NotFound.styles.js
+++ src/Components/NotFound/NotFound.styles.js
@@ -5,6 +5,9 @@ export const Wrapper = styled.div`
   height: 100%;
   padding: 0px 20px;
   font-size: var(--fontBig);
+  text-align: center;
+  margin-top: 20vh;
+
   p {
     font-size: var(--fontSmall);
   }
